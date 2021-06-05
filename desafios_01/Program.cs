f1ce7c47b5e7c97e1944101dc908e240cc03aad5
Eduardo Fernandes <ceduardodfernandes@gmail.com>
2021-06-05 18:26:02 -0300

    Initial commit

---
+++ desafios_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace desafios_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
