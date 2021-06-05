5ac732d2a317ec2b49fa8bae74323cb51a51c73f
Thainara Felix <thainarafelixv@hotmail.com>
2021-06-05 19:27:51 -0300

    Desafio 02

--- desafios_02/Program.cs
+++ desafios_02/Program.cs
@@ -2,11 +2,39 @@
 
 namespace desafios_02
 {
+    class Pessoa
+    {
+        public string nome;
+        public string idade;
+        public string sexo;
+
+    }    
+
     class Program
     {
+        
+        static void PrintPessoa(Pessoa pessoa)
+        {
+            Console.WriteLine(pessoa.nome);
+            Console.WriteLine(pessoa.sexo);
+            Console.WriteLine(pessoa.idade);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Qual o seu nome?");
+            string nome = Console.ReadLine();
+            Console.WriteLine ("Qual sua idade?");
+            string idade = Console.ReadLine();
+            Console.WriteLine("Qual o seu sexo?");
+            string sexo = Console.ReadLine ();
+
+            Pessoa pessoa = new Pessoa ();
+            pessoa.nome = nome;
+            pessoa.idade = idade;
+            pessoa.sexo = sexo;
+
+            PrintPessoa (pessoa);
         }
     }
 }
